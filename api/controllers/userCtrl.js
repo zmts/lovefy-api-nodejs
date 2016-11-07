0bd09e897a12e1855dbea4062a4ca2f6280f1bab
Sasha Zmts <zloy.root@gmail.com>
2016-11-07 13:21:23 +0200

    upd User model & controller

--- api/controllers/userCtrl.js
+++ api/controllers/userCtrl.js
@@ -4,16 +4,31 @@ var express = require('express'),
     router = express.Router(),
     User = require('../models/user');
 
-router.get('/all',              index());     // Show list of all users
-// router.post('/user',         create());    // Save user to the database.
-// router.get('/user/:id',      read());      // Display user details using the id
-// router.put('/user/:id',      update());    // Update details for a given user with id.
-// router.delete('/user/:id',   delete());    // Delete a given user with id.
-// router.get('/help',          help());      // Sends route help
+router.get('/help',         help());      // Sends route help
+router.get('/all',          getAll());    // Show list of all users
+router.post('/',            create());    // Save user to the database.
+router.get('/:id',          read());      // Display user details using the id
+// router.put('/:id',       update());    // Update details for a given user with id.
+// router.delete('/:id',    delete());    // Delete a given user with id.
 
 router.post('/checkNameAvailability', checkNameAvailability());
 
-function index() {
+
+/**
+ * url: user/help
+ * method: GET
+ */
+function help() {
+    return function(req, res) {
+        res.send('help info');
+    }
+}
+
+/**
+ * url: user/all
+ * method: GET
+ */
+function getAll() {
     return function(req, res) {
         User.fetchAll()
             .then(function(list) {
@@ -26,14 +41,56 @@ function index() {
     }
 }
 
+/**
+ * url: user/
+ * method: POST
+ * request: {"name": "value"}
+ */
+function create() {
+    return function(req, res) {
+        User.create(req.body)
+            .then(function (model) {
+                res.json(model);
+            })
+            .catch(function (error) {
+                res.send(error);
+            });
+    }
+}
+
+/**
+ * url: user/:id
+ * method: GET
+ */
+function read() {
+    return function(req, res) {
+        User.getById(req.params.id)
+            .then(function(user) {
+                if (user){
+                    res.json({success: true, data: user});
+                } else {
+                    res.json({success: false, error: "id " + req.params.id + " not found"});
+                }
+            })
+            .catch(function(error) {
+                res.send(error);
+            });
+    }
+}
+
+/**
+ * url: user/checkNameAvailability
+ * method: POST
+ * request: {"name": "value"}
+ */
 function checkNameAvailability() {
     return function(req, res) {
-        User.getName(req.body.name)
+        User.getByName(req.body.name)
             .then(function(model) {
                 if (model) {
-                    res.json({success: false, message: 'Not Available'});
+                    res.json({success: false, message: 'Not Available, name is taken'});
                 } else {
-                    res.json({success: true, message: 'Available'});
+                    res.json({success: true, message: 'Available, name is free'});
                 }
             }).catch(function(err) {
                 res.status(400).send(err.message);
--- api/models/user.js
+++ api/models/user.js
@@ -6,16 +6,49 @@ require('./post');
 
 var User = bookshelf.Model.extend({
     tableName: 'users',
+    hasTimestamps: true,
     posts: function() {
         return this.hasMany('Post');
+    },
+
+    initialize: function() {
+        this.on('fetched', this.validate);
+    },
+
+    validate: function () {
+        // console.log('model validation!!!!!');
+        // console.log(this.get('name'));
     }
+
 }, {
-        getName: Promise.method(function(value) {
+        getById: Promise.method(function(value) {
+            if (value) {
+                return this.forge().where('id', value).fetch();
+            } else {
+                throw new Error('Bad Request. Required {"id" : "value"} object in request is incorrect');
+            }
+        }),
+
+        getByName: Promise.method(function(value) {
             if (value) {
                     return this.forge().where('name', value).fetch();
                 } else {
                     throw new Error('Bad Request. Required {"name" : "value"} object in request is incorrect');
                 }
+        }),
+
+        create: Promise.method(function(data) {
+            new User({
+                    name: data.name,
+                    email: data.email
+                })
+                .save()
+                .then(function(model) {
+                    console.log(model.serialize())
+                })
+                .catch(function (error) {
+                    throw new Error(error);
+                });
         })
     }
 );

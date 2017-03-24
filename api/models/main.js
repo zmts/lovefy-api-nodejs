7636df00d0a43aa930495e1a2b4f20ede12b498f
Sasha Zmts <zloy.root@gmail.com>
2017-03-24 22:47:09 +0200

    task#39, MAIN model refactoring

--- api/models/main.js
+++ api/models/main.js
@@ -13,40 +13,14 @@ function MainModel() {
 
 Model.extend(MainModel);
 
-MainModel.create = function (data) {
-    return this.query().insert(data);
-};
-
 MainModel.CREATE = function (data) {
     return this.query().insert(data);
-};
-
-MainModel.getAll = function () {
-    return this.query()
-        .then(function (data) {
-            if (!data.length) throw { message: 'Empty response' };
-            return data;
-        })
-        .catch(function (error) {
-            throw error.message || error;
-        });
 };
 
 MainModel.GETall = function () {
     return this.query()
         .then(function (data) {
             if (!data.length) throw { message: 'Empty response' };
-            return data;
-        })
-        .catch(function (error) {
-            throw error.message || error;
-        });
-};
-
-MainModel.getById = function (id) {
-    return this.query().findById(id)
-        .then(function (data) {
-            if (!data) throw { message: 'Empty response' };
             return data;
         })
         .catch(function (error) {
@@ -65,16 +39,8 @@ MainModel.GETbyId = function (id) {
         });
 };
 
-MainModel.update = function (id, data) {
-    return this.query().patchAndFetchById(id, data);
-};
-
 MainModel.UPDATE = function (id, data) {
     return this.query().patchAndFetchById(id, data);
-};
-
-MainModel.remove = function (id) {
-    return this.query().deleteById(id);
 };
 
 MainModel.REMOVE = function (id) {

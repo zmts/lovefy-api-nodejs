ec2a733a781b76f1389e847ca1ae0856a521e6c9
Sasha Zmts <zloy.root@gmail.com>
2017-04-06 18:16:24 +0300

    JSdoc upd

--- api/controllers/postCtrl.js
+++ api/controllers/postCtrl.js
@@ -10,26 +10,42 @@ const auth = require('../middleware/auth');
 const sec = require('../middleware/security');
 
 /**
- * ------------------------------
+ * ------------------------------------------------------------
  * @BASE_URL: posts/
- * ------------------------------
+ * ------------------------------------------------------------
  */
 
 /**
- * ------------------------------
+ * ------------------------------------------------------------
  * @RELATED_ROUTES
- * ------------------------------
+ * ------------------------------------------------------------
+ */
+
+/**
+ * @description create and attach Tag to Post
+ * @hasaccess OWNER, SU, ADMINROLES
+ * @request {"name": "string"}
  */
 router.post('/:id/create-attach-tag',
     auth.checkToken(),
     sec.checkItemAccess.tokenUIDisEqualsModelUID(Post),
     createAndAttachTagToPost()
 );
+
+/**
+ * @description attach Tag to Post
+ * @hasaccess owner, SU, ADMINROLES
+ */
 router.post('/:id/attach-tag/:tag_id',
     auth.checkToken(),
     sec.checkItemAccess.tokenUIDisEqualsModelUID(Post),
     attachTagToPost()
 );
+
+/**
+ * @description detach Tag From Post
+ * @hasaccess owner, SU, ADMINROLES
+ */
 router.post('/:id/detach-tag/:tag_id',
     auth.checkToken(),
     sec.checkItemAccess.tokenUIDisEqualsModelUID(Post),
@@ -37,30 +53,60 @@ router.post('/:id/detach-tag/:tag_id',
 );
 
 /**
- * ------------------------------
+ * ------------------------------------------------------------
  * @BASE_ROUTES
- * ------------------------------
+ * ------------------------------------------------------------
+ */
+
+/**
+ * @description get all POST's list
+ * @url GET: posts?page=0
+ * @return ADMINROLES >> fetch all mix POSTS's of all users
+ * @return not ADMINROLES >> fetch all public POST's of all users
  */
 router.get('/',
     auth.checkToken(),
     sec.isAdmin(),
     getAll()
 );
+
+/**
+ * @description get Post by id
+ * @hasaccess public posts for All
+ * @hasaccess private posts for OWNER, SU, ADMINROLES
+ */
 router.get('/:id',
     auth.checkToken(),
     sec.checkItemAccess.read(Post),
     getPost()
 );
+
+/**
+ * @description create Post
+ * @hasaccess OWNER, SU, ADMINROLES
+ * @request {"user_id": "int", "title": "string", "content": "string"}
+ */
 router.post('/',
     auth.checkToken(),
     sec.checkItemAccess.create(),
     newPost()
 );
+
+/**
+ * @description update Post by id
+ * @hasaccess OWNER, SU, ADMINROLES
+ * @request {"title": "string", "content": "string"}
+ */
 router.patch('/:id',
     auth.checkToken(),
     sec.checkItemAccess.update(Post),
     update()
 );
+
+/**
+ * @description remove Post from db by id
+ * @hasaccess OWNER, SU, ADMINROLES
+ */
 router.delete('/:id',
     auth.checkToken(),
     sec.checkItemAccess.tokenUIDisEqualsModelUID(Post),
@@ -68,17 +114,11 @@ router.delete('/:id',
 );
 
 /**
- * ------------------------------
+ * ------------------------------------------------------------
  * @CONTROLLERS
- * ------------------------------
+ * ------------------------------------------------------------
  */
 
-/**
- * @description create and attach Tag to Post
- * @hasaccess OWNER, SU, ADMINROLES
- * @url POST: posts/:id/create-attach-tag
- * @request {"name": "string"}
- */
 function createAndAttachTagToPost() {
     return function (req, res, next) {
         delete req.body.helpData;
@@ -92,11 +132,6 @@ function createAndAttachTagToPost() {
     };
 }
 
-/**
- * @description attach Tag to Post
- * @hasaccess owner, SU, ADMINROLES
- * @url POST: posts/:id/attachTag/:tag_id
- */
 function attachTagToPost() {
     return function (req, res, next) {
         Post.CheckTagByIdInPost(req.params.id, req.params.tag_id)
@@ -109,11 +144,6 @@ function attachTagToPost() {
     };
 }
 
-/**
- * @description detach Tag From Post
- * @hasaccess owner, SU, ADMINROLES
- * @url POST: posts/:id/detachTag/:tag_id
- */
 function detachTagFromPost () {
     return function (req, res, next) {
         Post.DetachTagFromPost(req.params.id, req.params.tag_id)
@@ -123,12 +153,6 @@ function detachTagFromPost () {
     };
 }
 
-/**
- * @description get all POST's list
- * @url GET: posts?page=0
- * @return ADMINROLES >> fetch all mix POSTS's of all users
- * @return not ADMINROLES >> fetch all public POST's of all users
- */
 function getAll() {
     return function (req, res, next) {
         _getAllAccessSwitcher(req.body.helpData.isAdmin, +req.query.page)
@@ -149,12 +173,6 @@ function _getAllAccessSwitcher(isAdmin, queryPage) {
     return Post.GetPubList(queryPage);
 }
 
-/**
- * @description create Post
- * @hasaccess OWNER, SU, ADMINROLES
- * @url POST: posts/
- * @request {"user_id": "int", "title": "string", "content": "string"}
- */
 function newPost() {
     return function (req, res, next) {
         delete req.body.helpData;
@@ -165,12 +183,6 @@ function newPost() {
     };
 }
 
-/**
- * @description get Post by id
- * @hasaccess public posts for All
- * @hasaccess private posts for OWNER, SU, ADMINROLES
- * @url GET: posts/:id
- */
 function getPost() {
     return function (req, res, next) {
         Post.GetById(req.params.id)
@@ -180,12 +192,6 @@ function getPost() {
     };
 }
 
-/**
- * @description update Post by id
- * @hasaccess OWNER, SU, ADMINROLES
- * @url PATCH: posts/:id
- * @request {"title": "string", "content": "string"}
- */
 function update() {
     return function (req, res, next) {
         delete req.body.helpData;
@@ -196,11 +202,6 @@ function update() {
     };
 }
 
-/**
- * @description remove Post from db by id
- * @hasaccess OWNER, SU, ADMINROLES
- * @url DELETE posts/:id
- */
 function remove() {
     return function (req, res, next) {
         Post.REMOVE(req.params.id)

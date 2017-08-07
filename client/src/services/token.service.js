261a6350763aa990fe25babb0558b43791b97f74
Sasha Zmts <zloy.root@gmail.com>
2017-08-07 19:20:06 +0300

    upd client: removed token service

--- client/src/services/token.service.js
+++ client/src/services/token.service.js
@@ -1,57 +1,6 @@
-import jwtDecode from 'jwt-decode'
-import $store from '../store'
+// import $store from '../store'
 
 export default {
 
-    /**
-     * if  access token is valid >> decode
-     * else return false
-     */
-    decodeToken () {
-        let decodedAccessToken = ''
-        try {
-            // decode access token
-            decodedAccessToken = jwtDecode(localStorage.getItem('accessToken'))
-        } catch (error) {
-            console.log(error)
-        }
-
-        // if token is expired
-        if (decodedAccessToken && decodedAccessToken.exp <= Math.round(new Date().getTime() / 1000)) {
-            $store.commit('SET_ACCESS_TOKEN', false)
-            return decodedAccessToken
-        } else { // token is valid
-            $store.commit('SET_ACCESS_TOKEN', true)
-            return decodedAccessToken
-        }
-    },
-
-    decodeRefreshToken () {
-        let decodedRefreshToken = ''
-        try {
-            // decode refresh token
-            decodedRefreshToken = jwtDecode(localStorage.getItem('refreshToken'))
-        } catch (error) {
-            console.log(error)
-        }
-
-        if (decodedRefreshToken && decodedRefreshToken.exp <= Math.round(new Date().getTime() / 1000)) {
-            return false
-        } else {
-            return decodedRefreshToken
-        }
-    },
-
-    /**
-     * commit decoded user data to store
-     */
-    setUserData () {
-        $store.commit('SET_USER', {
-            id: this.decodeToken().sub,
-            role: this.decodeToken().userRole,
-            name: this.decodeToken().username,
-            email: this.decodeToken().email
-        })
-    }
 }
 

68f8aba61a3fbd8ff08670af9295943f216caca2
Sasha Zmts <zloy.root@gmail.com>
2017-08-15 22:32:43 +0300

    Client: finish refresh token service

--- client/src/services/http.init.js
+++ client/src/services/http.init.js
@@ -6,8 +6,8 @@
 
 import axios from 'axios'
 
-import App from '../app.init'
-// import authService from '../services/auth.service'
+// import App from '../app.init'
+import authService from '../services/auth.service'
 import $store from '../store'
 
 export default (status) => {
@@ -17,46 +17,32 @@ export default (status) => {
     if (this.isAuth) {
         this.instance.interceptors.request.use(request => {
             request.headers['token'] = localStorage.getItem('accessToken')
-            // if access token expired
-            // go to API and get new access token
+            // if access token expired >> go to API and get new access token
             if ($store.state.accessTokenExpDate <= (Math.floor(new Date().getTime() / 1000))) {
-                // authService.refreshTokens()
-                //     .then(res => {
-                //         localStorage.setItem('refreshToken', res.data.refreshToken)
-                //         localStorage.setItem('accessToken', res.data.accessToken)
-                //         request.headers['token'] = res.data.accessToken
-                //         $store.commit('SET_ATOKEN_EXP_DATE', res.data.expires_in)
-                //     })
-                //     .catch(error => {
-                //         if (error.response.data.badRefreshToken) {
-                //             console.log(error)
-                //             console.log('badRefreshToken: true')
-                //         }
-                //         if (error.response.data.refreshTokenExpiredError) {
-                //             console.log('refreshTokenExpiredError: true, hide profile button')
-                //         }
-                //     })
-                // console.log('new req')
-                return request
+                return authService.refreshTokens()
+                    .then(res => {
+                        localStorage.setItem('refreshToken', res.data.refreshToken)
+                        localStorage.setItem('accessToken', res.data.accessToken)
+                        request.headers['token'] = res.data.accessToken
+                        $store.commit('SET_ATOKEN_EXP_DATE', res.data.expires_in)
+
+                        return request
+                    })
+                    .catch(error => {
+                        if (error.response.data.badRefreshToken) {
+                            console.log(error)
+                            console.log('badRefreshToken: true')
+                            // App.$router.push('login')
+                        }
+                        if (error.response.data.refreshTokenExpiredError) {
+                            console.log('refreshTokenExpiredError: true, hide profile button')
+                            // App.$router.push('login')
+                        }
+                    })
             }
 
             return request
         }, error => {
-            return Promise.reject(error)
-        })
-
-        this.instance.interceptors.response.use(response => {
-            return response
-        }, error => {
-            if (error.response.data.badRefreshToken) {
-                console.log('badRefreshToken: true')
-                App.$router.push('login')
-            }
-
-            if (error.response.data.refreshTokenExpiredError) {
-                console.log('refreshTokenExpiredError: true')
-                App.$router.push('login')
-            }
             return Promise.reject(error)
         })
 

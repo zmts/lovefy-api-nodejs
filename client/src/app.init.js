184197cb2a01578468c34d20db1be112cd15bc05
Sasha Zmts <zloy.root@gmail.com>
2017-09-04 23:06:23 +0300

    minor/promise fix

--- client/src/app.init.js
+++ client/src/app.init.js
@@ -1,19 +1,24 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
+// import Promise from 'bluebird';
+// overwrite native Promise implementation with Bluebird's (for axios)
+// window.Promise = Promise;
+
+// global imports
 import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import Quill from 'vue-quill'
+import VueQuillEditor from 'vue-quill-editor'
+
+// app imports
 import AppLayout from './app.layout'
 import router from './app.router'
 import store from './store'
 import authService from './services/auth.service'
 import userService from './services/user.service'
 
-import VueMaterial from 'vue-material'
-import Quill from 'vue-quill'
-
-// import Promise from 'bluebird';
-// overwrite native Promise implementation with Bluebird's (for axios)
-// window.Promise = Promise;
-
+// styles imports
 import 'vue-material/dist/vue-material.css'
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
@@ -22,6 +27,7 @@ import 'quill/dist/quill.bubble.css'
 import './scss/style.scss'
 
 Vue.use(VueMaterial)
+Vue.use(VueQuillEditor)
 Vue.use(Quill)
 
 Vue.config.productionTip = false
@@ -55,10 +61,10 @@ export default new Vue({
                     store.commit('SET_ATOKEN_EXP_DATE', res.data.expires_in)
                 })
                 .then(() => {
-                    userService.getCurrentUser()
-                        .then(user => {
-                            store.commit('SET_USER', user.data.data)
-                        }).catch(error => console.log(error))
+                    return userService.getCurrentUser()
+                })
+                .then(user => {
+                    store.commit('SET_USER', user.data.data)
                 })
                 .catch(error => {
                     if (error.response.data.badRefreshToken) {
@@ -66,6 +72,8 @@ export default new Vue({
                     }
                     if (error.response.data.refreshTokenExpiredError) {
                         console.log('app.init.js >> refreshTokenExpiredError: true')
+                    } else {
+                        console.log(error)
                     }
                 })
         }
